80bc0128bc12881f2e42c9852eada9a5ebbfa099
Simone Monetti <simone.monetti@gmail.com>
2015-07-09 17:44:40 +0200

    refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
-var gulp = require('gulp');
-var compass = require('gulp-compass'),
-    minifyCSS = require('gulp-minify-css');
-var sourcemaps = require('gulp-sourcemaps');
-var connect = require('gulp-connect');
-var iconfont = require('gulp-iconfont');
-var iconfontCss = require('gulp-iconfont-css');
-var consolidate = require('gulp-consolidate');
+var gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    compass = require('gulp-compass'),
+    minifyCSS = require('gulp-minify-css'),
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    iconfont = require('gulp-iconfont'),
+    iconfontCss = require('gulp-iconfont-css'),
+    consolidate = require('gulp-consolidate');
 
 // Compass (with Sourcemaps and CSS Minifier)
 gulp.task('compass', function() {
@@ -13,20 +16,60 @@ gulp.task('compass', function() {
     .pipe(sourcemaps.init())
     .pipe(compass({
       config_file: './config.rb',
-      css: 'app/assets/css',
-      sass: 'app/assets/scss'
+      css: './app/assets/css',
+      sass: './app/assets/scss'
     }))
     .on('error', function(error) {
       // Would like to catch the error here
-      console.log(error);
-      this.emit('end');
+      browserSync.notify
     })
     .pipe(sourcemaps.write())
     .pipe(minifyCSS())
-    .pipe(gulp.dest('app/assets/css'));
+    .pipe(gulp.dest('./site/css'));
 });
 
-// Iconfont
+/**
+* Compile files from assets/js into site/js (for live injecting)
+ */
+gulp.task('js', function(){
+    return gulp.src('app/assets/js/*.js')
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('site/js'))
+        .pipe(browserSync.reload({stream:true}));
+});
+
+/**
+* Compile files from assets/vendor into site/js, site/css ecc (for live injecting)
+ */
+gulp.task('js:vendor', function(){
+    return gulp.src([
+            'app/assets/vendor/cookies-enabler/cookies-enabler.min.js'
+        ])
+        .pipe(concat('vendor.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('site/js'))
+        .pipe(browserSync.reload({stream:true}));
+});
+
+/**
+ * Copy files from assets/img into site/img (for live injecting)
+ */
+gulp.task('img', function() {
+    return gulp.src(['app/assets/img/*'])
+        .pipe(gulp.dest('site/img'))
+        .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('html', function() {
+    return gulp.src('app/*.html')
+      .pipe(gulp.dest('site'))
+      .pipe(browserSync.reload({stream:true}));
+});
+
+/**
+ * Copy files from assets/img into site/img (for live injecting)
+ */
 gulp.task('Iconfont', function(){
   gulp.src(['./app/assets/icons/*.svg'])
     .pipe(iconfontCss({
@@ -40,24 +83,33 @@ gulp.task('Iconfont', function(){
       //appendUnicode: true, // recommended option
       normalize: true
      }))
-    .pipe(gulp.dest('app/assets/fonts/'));
+    .pipe(gulp.dest('site/fonts/'));
 });
 
-// Connect
-gulp.task('connect', function() {
-	connect.server({
-    root: 'app',
-		port: 8080,
-		livereload: true
-	});
+/**
+ * Launch the Server
+ */
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: 'site'
+        }
+    });
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./app/assets/icons/*.svg', ['Iconfont']);
-  gulp.watch('./app/assets/scss/**/*.scss', ['compass']);
-  gulp.watch('./app/assets/scss/*.scss', ['compass']);
+  gulp.watch(['app/assets/icons/*.svg'], ['Iconfont']);
+  gulp.watch(['app/assets/scss/*.scss', 'app/assets/scss/**/*.scss'], ['compass']);
+  gulp.watch(['app/assets/js/*.js', 'app/assets/js/**/*.js'], ['js']);
+  gulp.watch(['app/assets/img/*'], ['img']);
+  gulp.watch(['app/*.html'], ['html']);
 });
 
+/**
+ * COMMAND-LINE TASKS
+ */
+
+gulp.task('default', ['html', 'js', 'js:vendor', 'compass', 'img', 'browser-sync', 'watch']); //Default task, running just `gulp` will compile the assets and compile the site
 gulp.task('fonts', ['Iconfont']);
 
-gulp.task('default', ['compass', 'connect', 'watch']);
+//gulp.task('default', ['compass', 'connect', 'watch']);
